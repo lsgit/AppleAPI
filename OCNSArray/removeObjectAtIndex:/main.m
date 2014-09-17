b4caa24aca3a51a4adbd133b2a32cf863619aad9
lsgit <848949609@qq.com day06 lsgit>
2014-09-17 18:40:46 +0800

    NSArray init

---
+++ OCNSArray/removeObjectAtIndex:/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  removeObjectAtIndex:
+//
+//  Created by liushuai on 14-9-17.
+//  Copyright (c) 2014年 Liushuai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
